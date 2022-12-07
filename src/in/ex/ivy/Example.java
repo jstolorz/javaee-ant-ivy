7a314495aeb852b30e25901806e2fc8735b556c8
Janusz Stolorz <jstolorz@gmail.com>
2022-12-07 16:32:10 +0100

    update_07122022

---
+++ src/in/ex/ivy/Example.java
@@ -0,0 +1,14 @@
+package in.ex.ivy;
+
+import org.apache.commons.lang.StringUtils;
+
+public class Example {
+  
+   public static void main(String[] args) {
+    
+    String string = StringUtils.upperCase("Ivy Beginner Guide");
+    System.out.println(string);
+
+   }
+
+}
